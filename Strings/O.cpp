3023b2543fc1b70f647770d0d2407cc9026ff802
k4r1mw4l1d <Karimwalid9876@gmail.com>
2025-11-25 22:32:17 +0200

    11/25/2025

--- Strings/O.cpp
+++ Strings/O.cpp
@@ -1,18 +1,23 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 int main(){
-    long long n;
-    string s;
-    cin >> n >> s;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1 - i; j++) {
-            if (s[j] > s[j + 1]) {
-                char temp = s[j];
-                s[j] = s[j + 1];
-                s[j + 1] = temp;
-            }
+    int n;
+    scanf("%d", &n);
+    
+    int count[26] = {0};
+    
+    char c;
+    for (int i = 0; i < n; i++){
+        scanf(" %c", &c);
+        count[c - 'a']++;
+    }
+    
+    for (int i = 0; i < 26; i++){
+        for (int j = 0; j < count[i]; j++){
+            putchar('a' + i);
         }
     }
-    cout << s;
+    putchar('\n');
+    
+    return 0;
 }
